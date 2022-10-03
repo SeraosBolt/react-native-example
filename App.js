80675e6c1e7414ff12e72e626c5a1013598279d7
unknown <you@example.com>
2022-10-03 17:20:08 -0300

    Adicionada Imagem para teste de Imagens

--- App.js
+++ App.js
@@ -1,18 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+import image from './assets/icon.png.png'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text_color}>Hello World!</Text>
-      <Text style={styles.text_color} >Matheus Soares!</Text>
-      <Text style={styles.text_color} >Desenvolvedor Completo!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.card}>
+      <View style={styles.header}>
+        <Text style={styles.text_header}>
+          Matheus Soares Desenvolvedor Completo
+        </Text>
+      </View>
+      <View style={styles.container}>
+        <Text style={styles.text_color}>Hello World!</Text>
+        <Text style={styles.text_color} >Matheus Soares!</Text>
+        <Text style={styles.text_color} >Desenvolvedor Completo!</Text>
+        <Image style={styles.image} source={image}/>
+        <StatusBar style="auto" />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  card:{
+    flex: 1,
+    backgroundColor: '#254689',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   container: {
     flex: 1,
     backgroundColor: '#254689',
@@ -24,5 +40,24 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: 30,
     textAlign: 'center'
+  },
+  image:{
+    marginTop: 15,
+    borderRadius: 25,
+    width: 250,
+    height: 250
+  },
+  header:{
+    height: 80,
+    backgroundColor: 'white',
+    width: 420,
+    alignItems:'center',
+    justifyContent: 'center',
+    textAlign: 'center',
+  },
+  text_header:{
+    fontWeight: 'bold',
+    fontSize: 20
   }
+
 });
